16090c1241c6b102d0d6b6c8e7b4be5e321390ff
julian layen <julian@codecodex.fr>
2020-11-23 14:08:35 +0100

    Dump SQL V2

--- data/sql/dump.sql
+++ data/sql/dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Nov 03, 2020 at 08:22 AM
+-- Generation Time: Nov 23, 2020 at 01:08 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
@@ -26,6 +26,14 @@ CREATE TABLE `brand` (
   `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `brand`
+--
+
+INSERT INTO `brand` (`id`, `name`, `deleted_at`) VALUES
+(2, 'Apple', '2020-11-03 08:33:59'),
+(3, 'Dell', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -37,6 +45,13 @@ CREATE TABLE `category` (
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `name`, `deleted_at`) VALUES
+(1, 'Informatique', NULL);
 
 -- --------------------------------------------------------
 
@@ -108,6 +123,17 @@ CREATE TABLE `product` (
   `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `brand_id`, `name`, `description`, `picture_first`, `picture_second`, `picture_third`, `price_ht`, `price_ttc`, `crated_at`, `updated_at`, `deleted_at`) VALUES
+(1, 2, 'dsfdfdsf', 'dsdsdsf', 'sdfdsds', 'fdsfdsfdf', 'sdsfdsf', 5, 5, '2020-11-03 09:10:57', NULL, '2020-11-03 11:48:07'),
+(2, 3, 'sdfdsdsfdsf', 'dsffddsfdsf', '400-320-5fa136f0b85ac.png', NULL, NULL, 5, 5, '2020-11-03 10:54:40', NULL, NULL),
+(3, 2, 'xcvxcgfgfgd', 'fgdgfdfgd', '400-320-5fa13ec5b51e8.png', '400-320-5fa13ec5b53e2.png', '400-320-5fa13ec5b5586.png', 5, 5, '2020-11-03 11:28:05', NULL, NULL),
+(4, 2, 'fgfghhgf', 'ghfghfghf', 'Example-5fa13f4acf714.jpeg', 'Example-5fa13f4acf948.jpeg', 'Example-5fa13f4acfb22.jpeg', 5, 5, '2020-11-03 11:30:18', NULL, NULL),
+(5, 2, 'dfgfddfg', 'fgdfgdfdgfgd', 'Example-5fa13fc3403c0.jpeg', NULL, NULL, 5, 5, '2020-11-03 11:32:19', NULL, '2020-11-03 11:36:28');
+
 -- --------------------------------------------------------
 
 --
@@ -118,6 +144,17 @@ CREATE TABLE `product_sub_category` (
   `product_id` int(11) NOT NULL,
   `sub_category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `product_sub_category`
+--
+
+INSERT INTO `product_sub_category` (`product_id`, `sub_category_id`) VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 1);
 
 -- --------------------------------------------------------
 
@@ -145,6 +182,13 @@ CREATE TABLE `sub_category` (
   `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `sub_category`
+--
+
+INSERT INTO `sub_category` (`id`, `category_id`, `name`, `deleted_at`) VALUES
+(1, 1, 'Son', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -169,7 +213,9 @@ INSERT INTO `user` (`id`, `email`, `roles`, `password`, `firstname`, `lastname`,
 (1, 'jyjebicy@mailinator.com', '[\"ROLE_CUSTOMER\",\"ROLE_ADMIN\"]', 'Suscipit elit corpo', 'Hayes', 'Alvarez', NULL),
 (2, 'gopupur@mailinator.com', '[\"ROLE_ADMIN\"]', 'Pa$$w0rd!qcxwwxc', 'Jeanette', 'Page', NULL),
 (3, 'zozof@mailinator.com', '[\"ROLE_ADMIN\"]', '$argon2id$v=19$m=65536,t=4,p=1$vtt4tfGpZ1huwmHNElic1A$UQiOUAjV6mZG3lzT14jkWrKFU5IQ0qOGDh4DiCLoYSk', 'Wade', 'Robles', NULL),
-(4, 'xobyz@mailinator.com', '[\"ROLE_ADMIN\"]', '$argon2id$v=19$m=65536,t=4,p=1$eym3eJEEjAWQVKe7BHE6Kw$3tMs95iRDe5149tGb4oNpiJeEr70o6DIjyjeKxsT7lQ', 'Ishmael', 'Chambers', NULL);
+(4, 'xobyz@mailinator.com', '[\"ROLE_ADMIN\"]', '$argon2id$v=19$m=65536,t=4,p=1$eym3eJEEjAWQVKe7BHE6Kw$3tMs95iRDe5149tGb4oNpiJeEr70o6DIjyjeKxsT7lQ', 'Ishmael', 'Chambers', NULL),
+(5, 'xobyz2@mailinator.com', '[\"ROLE_ADMIN\"]', '$argon2id$v=19$m=65536,t=4,p=1$hiDX5v5gSTSSTg64I+lgLg$0eSrBmAEk0EuhlQZN8/7GEboeH39xJ+OxgQDj1y3VFM', 'Jean', 'Durmeson', NULL),
+(6, 'aaaa@google.com', '[\"ROLE_ADMIN\"]', '$argon2id$v=19$m=65536,t=4,p=1$qbgYjzAhC4ENqAAwzYOEYQ$5zhJw12Xh4IvBqSA2mG2NaHlZ16cg09WMRuSwgXHYSU', 'AAA', 'AAA', NULL);
 
 --
 -- Indexes for dumped tables
@@ -252,13 +298,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `brand`
 --
 ALTER TABLE `brand`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `command`
@@ -276,7 +322,7 @@ ALTER TABLE `item`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `stock`
@@ -288,13 +334,13 @@ ALTER TABLE `stock`
 -- AUTO_INCREMENT for table `sub_category`
 --
 ALTER TABLE `sub_category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
