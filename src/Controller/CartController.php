a2139e24c31b87b1a6a5cfaea6f334e5499b2b04
julian layen <julian@codecodex.fr>
2020-12-14 17:44:22 +0100

    Stripe

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Item;
+use App\Entity\Order;
 use App\Entity\Product;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,6 +27,18 @@ class CartController extends AbstractController
         /** @var User $user */
         $user = $this->getUser();
         $command = $user->getOrdersByStatus(0);
+
+        if (!$command) {
+            $command = new Order();
+            $command->setCratedAt(new \DateTime());
+            $command->setUpdatedAt(new \DateTime());
+            $command->setUser($user);
+            $command->setStatus(0);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($command);
+            $em->flush();
+        }
 
         $item = new Item();
         $item->setQuantity($data['quantity']);
@@ -54,13 +67,53 @@ class CartController extends AbstractController
      */
     public function checkout(Request $request)
     {
+        // Get Entity Manager
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+
+        // Set Stripe Api Key
         \Stripe\Stripe::setApiKey('sk_test_XzLnJG2tgMhnzYAdietJqW1f');
 
+        // Get Cart
         $command = $this->getUser()->getOrdersByStatus(0);
 
-        // Variable POST
+        // Get Post vars
+        $data = $request->request->all();
 
-        return $this->render('cart/checkout.html.twig', ['command' => $command, "publishable_key" => "pk_test_NEDk9nXXSYacB5siSbRWziMo", 'amount' => $command->totalTTC() * 100 ]);
+        // Get Amount
+        $amount = $command->totalTTC() * 100;
+
+        // Check Stripe validation
+        if (isset($data['stripeToken']) && isset($data['stripeEmail'])) {
+
+            $customer = \Stripe\Customer::create([
+                'email' => $data['stripeEmail'],
+                'source'  => $data['stripeToken'],
+            ]);
+
+            $user->setStripeCustomerId($customer->id);
+            $em->persist($user);
+            $em->flush();
+
+            $charge = \Stripe\Charge::create([
+                'customer' => $customer->id,
+                'amount'   => $amount,
+                'currency' => 'eur',
+            ]);
+
+            if ($charge->status == 'succeeded') {
+                $command->setStatus(1);
+
+                $em->persist($command);
+                $em->flush();
+
+                return $this->redirectToRoute('cart.thanks');
+            } else {
+                return $this->redirectToRoute('cart.checkout');
+            }
+        }
+
+        return $this->render('cart/checkout.html.twig', ['command' => $command, "publishable_key" => "pk_test_NEDk9nXXSYacB5siSbRWziMo", 'amount' => $amount ]);
     }
 
     /**
@@ -74,4 +127,14 @@ class CartController extends AbstractController
 
         return $this->redirectToRoute('cart.checkout');
     }
+
+    /**
+     * @Route("/cart/thanks/", name="cart.thanks")
+     */
+    public function thanks(Request $request)
+    {
+
+        return $this->render('cart/thanks.html.twig');
+    }
+
 }
